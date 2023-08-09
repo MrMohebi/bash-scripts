7eff2847475ffd871532dfd2ac78f2b7c8e72b5d
MMMohebi <MMMohebi@outlook.com>
2023-08-09 14:10:25 +0330

    reboot-innodb.sh safe options

--- reboot-innodb.sh
+++ reboot-innodb.sh
@@ -3,6 +3,24 @@
 state=$1
 node=$2
 
+
+usage() {
+  __usage="
+  Usage: $(basename $0) [OPTIONS]
+
+  Options:
+    \$1, state name <string>      [required] state name(dev, prod ,etc)
+    \$2, node name  <string>      [required] node name(node1, node2 ,etc)
+  "
+  echo "$__usage"
+  exit 1;
+}
+
+if [ "$state" == '' ] || [ "$node" == '' ]; then
+  usage;
+  exit 0;
+fi
+
 containerName=$(docker ps --format "{{.Names}}" | grep "$state" | grep "$node")
 
 docker exec -it "$containerName" bash -c 'mysqlsh -uroot -p"$MYSQL_ROOT_PASSWORD" --js -e "dba.rebootClusterFromCompleteOutage()"'
